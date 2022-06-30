6488bfec1c549fae8b03d9cad47066fa3f65403e
Marco Castañeda <marco.dcc@gmail.com>
2022-06-29 22:09:48 -0500

    Sesion 01 Javascript

---
+++ js/base.js
@@ -0,0 +1,19 @@
+console.log("Hola Koders");
+
+
+// ! Datos en Js
+  // Identificador
+  // Reglas o buenas prácticas
+    // Las variables no deben de contener caractéres especiales (, ! @) -> let @@@ = 'test'
+    // No inician con digitos -> let 9variable = 9; -> Error
+    // No acepta palabras reservadas -> let else = 'else' => Error
+    // Buena practica; camelCase 99%  | PascalCase -> Clases | CONSTANTES
+    // Buena practica; Naming descriptivo -> let estudiantesKodemia = 'koders'
+    // Consideración; Sensible a las mayus y minus 
+
+// ! Variables y Constantes
+    // Variables = Mutabilidad
+    // Constantes = No cambia
+
+    // Variables - palabras reservadas para declarar; var y let
+    
