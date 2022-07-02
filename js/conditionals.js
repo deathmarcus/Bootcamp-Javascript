f8bad2560a5b2be13d8cba12d658877642c74f49
Marco Castañeda <marco.dcc@gmail.com>
2022-07-01 20:33:20 -0500

    Practica 1 Sesion 03 js

---
+++ js/conditionals.js
@@ -0,0 +1,68 @@
+// ! Conditionals
+  // Estructuras de control selectivas
+    // Simples
+
+    // const montoMinimo = 2000;
+    // let montoCompra = 1500;
+
+    // if(montoCompra >= montoMinimo) {
+    //     console.log('Elegible a MSI');
+    // }
+
+    // Doble
+
+    // const montoMinimo = 2000;
+    // let montoCompra = 1500;
+
+    // if(montoCompra >= montoMinimo) {
+    //     console.log('Elegible a MSI');
+    // }
+    // else{
+    //     console.log('MSI no estan disponibles');
+    // }
+
+    // Multiples
+
+    // const montoMinimo = 2000;
+    // let montoCompra = 1500;
+
+    // if(montoCompra >= montoMinimo) {
+    //     console.log('Elegible a MSI');
+    // }
+    // else if(montoCompra + 100 >= 2000){
+    //     console.log('Agrega $100 o mas a tu compra y accede a MSI');
+    // }else{
+    // console.log('MSI no estan disponibles');
+    // }
+
+    // Anidacion o nesting de condicionales
+
+    // const numero = parseInt(prompt('Ingresa un numero a evaluar'));
+    
+    // if(numero >= 0){
+         //codigo
+    //     if(num == 0){
+    //         console.log('El numero ingresado es 0');
+    //     }
+    //     else {
+    //         console.log('El numero ingresado es positivo');
+    //     }
+    // }
+    // else{
+    //     console.log('El numero es negativo!');
+    // }
+
+    // ! Pedir al usuario 3 números e imprimir en consola el mayor
+        console.log('Dame 3 numeros diferentes');
+        let numA = parseInt(prompt('Dame el primer numero:'));
+        let numB = parseInt(prompt('Dame el segundo numero:'));
+        let numC = parseInt(prompt('Dame el tercer numero:'));
+        if(numA == numB || numA == numC || numB == numC){
+            console.error('Los numeros tienen que ser distintos')
+        }else if(numA > numB && numA > numC){
+            console.log(`El mayor es el primer número: ${numA}`);
+        }else if(numB > numC){
+            console.log(`El mayor es el segundo número: ${numB}`);
+        }else {
+            console.log(`El mayor es el tercer número: ${numC}`)
+        }
