ce35ae5e144b875639f2ed72d28a27713e338b04
Marco Castañeda Laptop <marco.dcc@gmail.com>
2022-07-08 15:42:31 -0500

    Se añadio el archivo de funciones flecha

---
+++ js/arrowFunctions.js
@@ -0,0 +1,10 @@
+// ! Funciones Flecha o Arrow Functions
+// No necesitamos de la palabra reservada functions
+// no en todos los casos es necesaria la palabra
+// reservada return para regresar
+
+const arrowFunction = (p1, p2) => {
+    console.log(p1, p2);
+    return;
+};
+
