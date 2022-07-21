6f127de8839bcf8c67e26ddb72d9a784d9055067
Marco Castañeda <marco.dcc@gmail.com>
2022-07-20 22:15:32 -0500

    Sesion 14 Eventos

---
+++ js/events.js
@@ -0,0 +1,18 @@
+// ! Eventos
+    //Estos suceden sobre los elementos de nuestro DOM y podemos capturar y reaccionar a ellos, manipularlos a nuestro favor
+
+// ! Tipos de eventos
+    // Eventos automáticos
+    // Eventos generados por usuarios
+
+// ? Estilos en línea => Online events
+
+function imgGrande(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+  }
+  
+  function imgNormal(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+  }
